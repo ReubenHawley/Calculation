10aaf8019a1936d30b3665dc32289ce287bb7868
Reuben <reubenhawley@protonmail.com>
2022-06-19 20:00:06 +0200

    Initial commit

---
+++ AddNamesWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace MyFirstWPFApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddName_click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(txtName.Text) && !LstNames.Items.Contains(txtName.Name))
+            {
+                LstNames.Items.Add((txtName.Text));
+                txtName.Clear();
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace MyFirstWPFApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
